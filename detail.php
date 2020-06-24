26ed6c806e0244360c653404897cc1df385f857c
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 15:33:26 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -4,7 +4,7 @@
 
     // Agrega credenciales
     
-    MercadoPago\SDK::setAccessToken('TEST-7734609094343991-062405-3f95b97ed7f48d0de502a44910c075f3-590053504');
+    MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
     
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
@@ -18,7 +18,6 @@
     $item->quantity = $_POST['unit'];
     $item->unit_price = $_POST['price'];
     $item->external_reference = 'carlosgarcia@cbtis52.edu.mx';
-    $item->client_id = "535650015";
     $preference->items = array($item);
     $preference->save();
     
