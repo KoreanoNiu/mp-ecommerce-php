263950dacd119768bca1d624cb77a713bf5a2e17
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 01:49:16 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -176,5 +176,21 @@
     $url = "https://api.mercadolibre.com/users/test_user?access_token=" . $asd;
     $vars = '{\"site_id\":\"MLM\"}';
 
-    print_r($vars);
+    echo(enviar($vars, $url));
+
+    function enviar($parametrospost,$urlpost)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $urlpost);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $parametrospost);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $respuesta = trim(curl_exec($curl));
+        curl_close($curl);
+        
+        return $respuesta ;
+    } 
 ?>
