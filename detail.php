988d532ad4e5b533956b74d69e487a86177a529d
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 18:14:55 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -38,8 +38,6 @@
 
         $preference->payer = $payer;
 
-        $payment_methods = new MercadoPago\Payment_method();
-
         $preference->external_reference = 'carlosgarcia@cbtis52.edu.mx';
     
         $preference->back_urls= array(
