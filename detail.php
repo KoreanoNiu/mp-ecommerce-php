75bce09133e4035e56657558c3fd9bd8ade90d33
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 23:37:35 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -15,11 +15,14 @@
         $item->id = "1234";
         $item->title = $_POST['title'];
         $item->description = 'Dispositivo móvil de tienta e-commerce';
-        $item->picture_url = '<img src="https://koreanoniu-mp-ecommerce-php.herokuapp.com/' . substr($_POST['img'], 2) . '">' . "</img>";
+        $item->picture_url = 'https://koreanoniu-mp-ecommerce-php.herokuapp.com/' . substr($_POST['img'], 2);
         $item->quantity = $_POST['unit'];
         $item->unit_price = $_POST['price'];
+
+        //External reference (correo del formulario de examen)
+        $preference->external_reference = 'carlosgarcia@cbtis52.edu.mx';
     
-        //Crea un Payer
+        //Crea un Payer (Datos del cliente)
         $payer = new MercadoPago\Payer();
     
         $payer->name = "Lalo";
@@ -48,15 +51,17 @@
             "installments" => 6
         );
 
+        //Url para el webhook
         $preference->notification_url = "https://koreanoniu-mp-ecommerce-php.herokuapp.com/notification.php";
-        $preference->external_reference = 'carlosgarcia@cbtis52.edu.mx';
     
+        //Back url's correspondientes
         $preference->back_urls= array(
             "success" => "https://koreanoniu-mp-ecommerce-php.herokuapp.com/pago-aceptado.php",
             "failure" => "https://koreanoniu-mp-ecommerce-php.herokuapp.com/pago-cancelado.php",
             "pending" => "https://koreanoniu-mp-ecommerce-php.herokuapp.com/pago-pendiente.php"
         );
 
+        //Auto return
         $preference->auto_return = "approved";
 
         $preference->items = array($item);
@@ -218,11 +223,3 @@
         </div>
 
 </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div id="ac-gn-viewport-emitter"> </div></body></html>
-
-<?php 
-    echo('<img src="https://koreanoniu-mp-ecommerce-php.herokuapp.com/' . substr($_POST['img'], 2) . '">' . "</img>");
-
-    print_r($preference);
-
-    echo '<br>';
-?>
