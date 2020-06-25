fd578c931ca43232e339c550c2bfaa6789ad01d1
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-25 00:08:26 -0500

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,5 +1,4 @@
 <?php
-
     require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
@@ -11,7 +10,7 @@
     $actual = file_get_contents($fichero);
     
     $actual .= $entityBody;
-    
+
     file_put_contents($fichero, $actual);
 
 ?>
