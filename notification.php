bec618e057c782cee9f1642f8cd42ada3566780f
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 20:56:20 -0500

    Update notification.php

--- notification.php
+++ notification.php
@@ -4,5 +4,11 @@
 
     MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 
-    $entityBody = file_get_contents('php://input');
+    $entityBody = file_get_contents('php://notification.php');
+
+    $data = json_decode($entityBody, TRUE);
+    
+    $json_string = json_encode($data);
+    $file = 'clientes.json';
+    file_put_contents($file, $json_string);
 ?>
