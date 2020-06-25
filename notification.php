aa1988e793b21cf3dc1996543d8f0427089bb1bf
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 21:17:45 -0500

    Update notification.php

--- notification.php
+++ notification.php
@@ -10,7 +10,7 @@
     
     $arr_clientes = array('nombre'=> 'Jose', 'edad'=> '20', 'genero'=> 'masculino', 'email'=> 'correodejose@dominio.com', 'localidad'=> 'Madrid', 'telefono'=> '91000000');
 
-    $json_string = json_encode($arr_clientes);
+    $json_string = json_encode($data);
     $file = './clientes.json';
     file_put_contents($file, $json_string);
 
