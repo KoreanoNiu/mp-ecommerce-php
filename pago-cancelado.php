5d832bcb1e03579185e7a85130cb09e4893c458f
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-24 23:48:42 -0500

    Update pago-cancelado.php

--- pago-cancelado.php
+++ pago-cancelado.php
@@ -1,5 +1,7 @@
 <?php 
-    $url= $_SERVER["REQUEST_URI"];
 
-    echo '{' . $url . '}';
+    $json = file_get_contents('php://input');
+    $obj = json_decode($json);
+    echo $obj->collection_id;
+
 ?>
