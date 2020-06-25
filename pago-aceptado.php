16198a44b62c88ae40cd83bb4c6d3c97faf727cb
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-06-25 00:08:28 -0500

    Update pago-aceptado.php

--- pago-aceptado.php
+++ pago-aceptado.php
@@ -1,7 +1,7 @@
 <?php 
 
-    echo 'Payment type: ' . ($_GET['payment_type']);
-    echo 'External reference: ' . ($_GET['external_reference']);
-    echo 'Collection id: '. ($_GET['collection_id']);
+    echo 'Payment type: ' . ($_GET['payment_type']) .'<br>';
+    echo 'External reference: ' . ($_GET['external_reference']) . '<br>';
+    echo 'Collection id: '. ($_GET['collection_id']) . '<br>';
 
 ?>
